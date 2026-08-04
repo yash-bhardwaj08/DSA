55a88e81b8fbfbb7d757982eba946f585bf60627
Yash Bhardwaj <bhardwajy969@gmail.com>
2026-08-04 20:21:04 +0530

    Time: 1 ms (92.6%), Space: 45.2 MB (63.12%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -30,6 +30,7 @@ class Solution {
     return nums[nums.length - 1];
     }
 }*/
+/*
 class Solution {
     public int singleNumber(int[] nums) {
 
@@ -56,5 +57,17 @@ class Solution {
         }
 
         return ans;
+    }
+}*/
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0;
+        int twos = 0;
+
+        for(int i = 0; i<nums.length; i++){
+            ones = ((ones ^ nums[i]) & (~twos));
+            twos = ((twos ^ nums[i]) & (~ones));
+        }
+    return ones;
     }
 }
