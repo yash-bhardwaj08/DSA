6ff7d53d2f9cae394a55cbded1aa75f917fa6700
Yash Bhardwaj <bhardwajy969@gmail.com>
2026-08-11 10:33:28 +0530

    Time: 19 ms (83.56%), Space: 45.9 MB (31.99%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,66 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        for (int i = 0; i < n - 3; i++) {
+
+            // Skip duplicate first elements
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n - 2; j++) {
+
+                // Skip duplicate second elements
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+
+                    long sum = (long) nums[i] + nums[j]
+                             + nums[left] + nums[right];
+
+                    if (sum == target) {
+
+                        ans.add(Arrays.asList(
+                            nums[i],
+                            nums[j],
+                            nums[left],
+                            nums[right]
+                        ));
+
+                        left++;
+                        right--;
+
+                        // Skip duplicates
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+
+                    } 
+                    else if (sum < target) {
+                        left++;
+                    } 
+                    else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
