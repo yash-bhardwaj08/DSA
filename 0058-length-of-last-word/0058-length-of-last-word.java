8c84f85c61072260a531f7191f4930e09d7c9524
Yash Bhardwaj <bhardwajy969@gmail.com>
2026-08-15 12:11:20 +0530

    Time: 0 ms (100%), Space: 43.1 MB (52.33%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        for(int i = s.length()-1; i>= 0; i--){
+            if(s.charAt(i) == ' ' && count == 0){
+                continue;
+            }
+            
+            if(s.charAt(i) == ' '){
+                break;
+            }
+            count++;
+        }
+    return count;
+    }
+}
