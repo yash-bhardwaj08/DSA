31e22d18ea5d15fa381182455347a03678beb550
Yash Bhardwaj <bhardwajy969@gmail.com>
2026-09-09 00:34:06 +0530

    Time: 7 ms (67.84%), Space: 44.1 MB (48.13%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if(s1.length() > s2.length()){
+            return false;
+        }
+        
+        int[] freq = new int[26];
+        int[] freq2 = new int[26];
+
+        for(int i = 0; i<s1.length(); i++){
+            freq[s1.charAt(i) - 'a']++;
+        }
+    
+        int left = 0;
+        for(int right = 0; right<s2.length(); right++){
+            freq2[s2.charAt(right) - 'a']++;
+
+            if(right-left+1 > s1.length()){
+                freq2[s2.charAt(left) - 'a']--;
+                left++;
+            }
+            if (right - left + 1 == s1.length()) {
+                if (Arrays.equals(freq, freq2)) {
+                    return true;
+                }
+            }
+        }
+    return false;
+    }
+}
